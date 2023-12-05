2ef09a598e9b737f01dd60ca6c4f3fd38f060aaf
Artyom <artyomka_working@mail.ru>
2023-12-05 22:14:13 +0300

    update server

--- server.js
+++ server.js
@@ -24,32 +24,3 @@ app.use('/', function(request, response) {
 app.listen(port, function() {
     console.log("'Server is running on port " + port);
 });
-
-// db.query('SELECT * FROM QUIZ', function(error, result) {
-//     if (error) {
-//       console.log(error.message);
-//       db.end();
-//     } else {
-//       var rows = result.rows;
-//       var countRows = result.rowCount;
-
-//       for (var i = 0; i < countRows; ++i) {
-//         var question = rows[i];
-
-//         var descriptionQuestion = question.question;
-
-//         var answers = question.answers;
-//         var countAnswers = answers.length;
-
-//         console.log(descriptionQuestion, countAnswers);
-
-//         for (var j = 0; j < countAnswers; ++j) {
-//           var answer = answers[j];
-//           var descriptionAns = answer.answer;
-//           var correctAns = answer.correct;
-
-//           console.log(descriptionAns, correctAns);
-//         }
-//       }
-//     }
-//   });
