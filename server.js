6ca839ff45b1bf7ae572836a2b9a8cffbe1d5089
Artyom <artyomka_working@mail.ru>
2023-12-06 22:38:48 +0300

    parse

--- server.js
+++ server.js
@@ -11,7 +11,41 @@ app.post('/submit-test', function(request, response) {
     const selectedAnswers= request.body;
     console.log(selectedAnswers);
 
-    response.send('Данные успешно получены');
+    var countRequestAnswers =  Object.keys(selectedAnswers).length;
+    console.log(countRequestAnswers);
+
+    db.query("SELECT * FROM QUIZ", function(error, result){
+        if (error){
+            console.log(error.message);
+            response.send(error.message);
+        }
+
+        var rows = result.rows;
+        var countRows = result.rowCount;
+
+        for (var i = 0; i < countRows; ++i) {
+            var question = rows[i];
+            var descriptionQuestion = question.question;
+
+            var answers = question.answers;
+            var countAnswers = answers.length;
+
+            console.log(descriptionQuestion, countAnswers);
+
+            for (var j = 0; j < countAnswers; ++j) {
+                var answer = answers[j];
+                var descriptionAns = answer.answer;
+                var correctAns = answer.correct;
+                
+                console.log(descriptionAns, correctAns);
+            }
+        }
+        
+
+        response.send('Данные успешно получены');
+    })
+
+    //response.send('Данные успешно получены');
 });
 
 app.get('/test', function(request, response) {
@@ -22,13 +56,17 @@ app.get('/test', function(request, response) {
             response.send(error.message);
         }
 
+        test = result;
+
        response.render("index", {
         rows: result.rows,
         countRows: result.rowCount
        })
+
+       db.end();
     })
 });
 
 app.listen(port, function() {
-    console.log("Server is running on port " + port);
+    console.log("Сервер запущен по порту " + port);
 });
