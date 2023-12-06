0bfb4053c48b47317e61790ede62017950f3a306
Artyom <artyomka_working@mail.ru>
2023-12-06 21:35:48 +0300

    update

--- DB.js
+++ DB.js
@@ -1,11 +1,11 @@
-// const {Pool} = require('pg');
+const {Pool} = require('pg');
 
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'postgres',
-//   password: '12345',
-//   port: 5432
-// });
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '12345',
+  port: 5432
+});
 
-// module.exports = pool;
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,38 +1,25 @@
-const {Pool} = require('pg');
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: '12345',
-  port: 5432
-});
-
+const db = require("./DB");
 const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-// Добавим парсер для обработки данных формы
-app.use(express.urlencoded({ extended: true }));
-
-// Изменим обработчик маршрута на POST
 app.post('/submit-test', function(request, response) {
-    const selectedAnswerQ1 = request.body;
-    console.log(selectedAnswerQ1); // Для проверки
+    const selectedAnswers= request.body;
+    console.log(selectedAnswers);
 
-    // Отправим ответ об успешном получении данных
     response.send('Данные успешно получены');
 });
 
-
 app.get('/test', function(request, response) {
 
-    pool.query("SELECT * FROM QUIZ", function(error, result){
+    db.query("SELECT * FROM QUIZ", function(error, result){
         if (error){
             console.log(error.message);
+            response.send(error.message);
         }
 
        response.render("index", {
@@ -43,5 +30,5 @@ app.get('/test', function(request, response) {
 });
 
 app.listen(port, function() {
-    console.log("'Server is running on port " + port);
+    console.log("Server is running on port " + port);
 });
